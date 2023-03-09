429c4ef811cc54c3a068f745f9cd2f449b4c120b
CJTaylor <tka4inni@gmail.com>
2023-03-09 17:55:22 +0300

    09/03/23

--- argus-feedback/send.php
+++ argus-feedback/send.php
@@ -1,86 +1,38 @@
-<?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-header('Pragma: public');
-header('Access-Control-Allow-Origin: *');
-
-require './PHPMailer/Exception.php';
-require './PHPMailer/PHPMailer.php';
-require './PHPMailer/SMTP.php';
-
-    if (isset($_POST['name'])) {
-        $name = $_POST['name'];
+<?
+    if(isset($_POST['name'])) {
+        $name = '<p>name: '.$_POST['name'].'</p>';
     }
-    if (isset($_POST['phone'])) {
-        $phone = $_POST['phone'];
+    if(isset($_POST['phone'])) {
+        $phone = '<p>phone: '.$_POST['phone'].'</p>';
     }
-    if (isset($_POST['email'])) {
-        $email = $_POST['email'];
+    if(isset($_POST['email'])) {
+        $email = '<p>email: '.$_POST['email'].'</p>';
     }
-    if (isset($_POST['download-file'])) {
-        $downloadedFile = $_POST['download-file'];
-    }
-    if (isset($_POST['download-file'])) {
-        $downloadedFile = $_POST['download-file'];
+    if(isset($_POST['file'])) {
+        $file = '<p><b>Прикреп: </b>'.$_POST['file'].'</p>';
     }
 
-    $person = '';
+    $to = 'tka4inni@gmail.com';
 
-    switch ($downloadedFile) {
-        case 'Template':
-            $person = 'Предположительно скачал: Инженер';
-            break;
-        case 'List':
-            $person = 'Предположительно скачал: Секретарь';
-            break;
-        case 'Checklist':
-            $person = 'Предположительно скачал: Руководитель';
-            break;    
-    }
+    // $to = 'advertising@argus-eko.ru, margo@argus-eko.ru';
 
-    $heading = ($person === '') ? 'Отправили заявку с reg.argus' : 'Скачали КП + подарок (reg.argus)';
+    // $to = '126@argus-eko.ru';
 
-    $subject = $heading;
-    $message = "name: $name\n<br />\n<br />phone: $phone\n<br />\n<br />email: $email\n<br />\n<br />$heading\n<br />\n<br />$person";
-    $html = '<html><head><meta http-equiv="content-type" content="text/html; charset=utf-8"></head><body>'.$message.'</body></html>';
+    $subject = 'Отправили заявку с attestat';
+    $message = '
+                    <html>
+                        <head>
+                            <title>'.$subject.'</title>
+                        </head>
+                        <body>
+                            '. $name .'
+                            '. $phone .'
+                            '. $email .'
+                            '. $file .'
+                        </body>
+                    </html>';
+    $headers  = "Content-type: text/html; charset=utf-8 \r\n";
+    $headers .= "From: Argus <argus@argous.group>\r\n";
+    mail($to, $subject, $message, $headers);
 
-//Load Composer's autoloader
-$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
-try {
-    $mail->CharSet = 'UTF-8';
-    $mail->isSMTP();                                      // Set mailer to use SMTP
-    $mail->Host = 'smtp.yandex.com';                      // Specify main and backup SMTP servers
-    $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'argus@argus.group';                // SMTP username
-    $mail->Password = 'ARkeyf20';                         // SMTP password
-    $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-    $mail->Port = 465;                                    // TCP port to connect to
-
-    //Recipients
-    $mail->From='argus@argus.group';
-    $mail->FromName = 'Argus';
-    // $mail->addAddress("tka4inni@gmail.com", "АРГУС");     // Add a recipient
-
-    $recipients = array(
-        'advertising@argus-eko.ru' => 'АРГУС',
-        'margo@argus-eko.ru' => 'АРГУС',
-     );
-     foreach($recipients as $email => $name)
-     {
-        $mail->addAddress($email, $name);
-     }
-
-    //Content
-    $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = "=?UTF-8?B?".base64_encode($subject)."?=";
-    $mail->Body    = $html;
-    $mail->AltBody = $message;
-    $mail->send();
-    $return = true;
-    echo json_encode($return);
-    } 
-    catch (Exception $e) {
-    $return = false;
-    echo json_encode($return);
-}
 ?>
--- argus-feedback/sendFiles.php
+++ argus-feedback/sendFiles.php
@@ -17,37 +17,15 @@ if(isset($_POST['email'])) {
 if(isset($_POST['name'])) {
     $name = $_POST['name'];
 }
-if(isset($_POST['files'])) {
-    $files = $_POST["files"];
+if(isset($_POST['phone'])) {
+    $phone = $_POST['phone'];
 }
-if(isset($_POST['filesName'])) {
-    $fileNames = $_POST["filesName"];
-}
-
-$gift = '';
-$giftName = '';
-$kp = '';
-
-switch ($fileNames) {
-    case 'Template':
-        $gift = '"Шаблоны для заполнения отчётов"';
-        $giftName = 'Шаблоны.zip';
-        $kp = './assets/download/kp.pdf';
-        break;
-    case 'List':
-        $gift = '"Список бесплатных сервисов для офисной работы"';
-        $giftName = 'Список.pdf';
-        $kp = './assets/download/kp.pdf';
-        break;
-    case 'Checklist':
-        $gift = '"Чеклист по внедрению техники безопасности"';
-        $giftName = 'Чеклист.pdf';
-        $kp = './assets/download/kp.pdf';
-        break;
+if(isset($_POST['file'])) {
+    $file = $_FILES["file"];
 }
 
 $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
-$message = 'Уважаемый '.$name.'. Высылаем Вам коммерческое предложение по регистрации электролаборатории и наш подарок - '.$gift.' (в приложении этого письма). Надеемся на долгосрочное сотрудничество. Наш email: argus@argus-eko.ru и телефон: +7(495)585-09-82. Обращайтесь по любым вопросам.';
+$message = 'Уважаемый '.$name.'. '.$email.'Высылаем Вам коммерческое предложение по регистрации электролаборатории и наш подарок - '.$phone.' (в приложении этого письма). Надеемся на долгосрочное сотрудничество. Наш email: argus@argus-eko.ru и телефон: +7(495)585-09-82. Обращайтесь по любым вопросам.';
 $html = '<html><head><meta http-equiv="content-type" content="text/html; charset=utf-8"></head><body>'.$message.'<br/><br/></body></html>';
 try {
     $mail->CharSet = 'UTF-8';
@@ -55,18 +33,25 @@ try {
     $mail->Host = 'smtp.yandex.com';                      // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
     $mail->Username = 'argus@argus.group';                // SMTP username
-    $mail->Password = 'ARkeyf20';                         // SMTP password
+    $mail->Password = 'ARkeyf31';                         // SMTP password
     $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 465;                                    // TCP port to connect to
 
     //Recipients
     $mail->From='argus@argus.group';
     $mail->FromName='Группа компаний АРГУС';
-    $mail->addAddress($email);                            // Add a recipient
+    $mail->addAddress("tka4inni@gmail.com", "АРГУС");                            // Add a recipient
 
     //Attachments
-    $mail->addAttachment($kp, 'КП АРГУС.pdf', 'base64');
-    $mail->addAttachment($files, $giftName, 'base64');
+    // $mail->addAttachment($kp, 'КП АРГУС.pdf', 'base64');
+    // $mail->addAttachment($file, 'base64');
+
+    // if (isset($_FILES['file'])
+    // && $_FILES['file']['error'] == UPLOAD_ERR_OK
+    // ) {
+    // $mail->addAttachment($_FILES['file']['tmp_name'],
+    //                      $_FILES['file']['name']);
+    // }
 
     //Content
     $mail->isHTML(true);
@@ -80,4 +65,6 @@ try {
     $return = false;
     echo json_encode($return);
 }
+
+echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
 ?>
